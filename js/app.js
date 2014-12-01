3b1f6b1f095ea6dfef35cb27597acd1818e5c42a
Chris <christopher.reade@gmail.com>
2014-11-30 22:30:08 -0600

    styling

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     ).done(function(teamData, firstNames, lastNames, playData, kickdata, logos) {
         var seed = Math.random().toString(36).substring(7);
         console.log(seed)
-        Math.seedrandom("3hobax20529");
+        Math.seedrandom(seed);
 
         var lastNameGenerator = genball.generators.lastNameGenerator(lastNames);
         var firstNameGenerator = genball.generators.firstNameGenerator(firstNames);
